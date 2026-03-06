312f59b76527c7bb5aa75750704829668d2480eb
mohamed_bnz <simobnz81@gmail.com>
2026-03-06 10:04:23 +0000

    ajouter la fonction app qui faire la laison entre les fichie

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,40 @@ import TournamentDetails from './components/TournamentDetails';
 
 
 
+function App() {
+
+  const [selectedTournament, setSelectedTournament] = useState(null);
+
+  return (
+    <div className="min-h-screen bg-slate-50">
+
+      {!selectedTournament && <Header />}
+      <main className=" max-w-xl pb-10">
+
+        {!selectedTournament ? (
+
+          tournamentData.map((t) => (
+            <TournamentCard
+              key={t.id}
+              tournament={t}
+              onClick={() => setSelectedTournament(t)}
+            />
+          ))
+
+        ) 
+        :
+        (
+          <TournamentDetails
+            tournament={selectedTournament}
+            goBack={() => setSelectedTournament(null)}
+          />
+
+        )}
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
