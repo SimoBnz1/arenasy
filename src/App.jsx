9d2fe19709a775154be94c3f4f4535b9c9431bd6
mohamed_bnz <simobnz81@gmail.com>
2026-03-06 10:03:26 +0000

    faire l importation des fichier dans app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { tournamentData } from './data/tournamentDB';
+import TournamentCard from './components/TournamentCard';
+import Header from './components/Header';
+import TournamentDetails from './components/TournamentDetails';
+
+
+
