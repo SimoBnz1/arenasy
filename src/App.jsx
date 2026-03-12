f83d85ac78c1059cf4e72dff0228b951a486799f
mohamed_bnz <simobnz81@gmail.com>
2026-03-12 11:38:54 +0000

    ajouter le state de recherche et la logique pour faire la recherch

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,38 @@ import TournamentDetails from './components/TournamentDetails';
 function App() {
 
   const [selectedTournament, setSelectedTournament] = useState(null);
+  const [search, setSearch] = useState("");
+  const [tournaments, setTournaments] = useState(tournamentData);
+
+
 
   return (
     <div className="min-h-screen bg-slate-50">
 
-      {!selectedTournament && <Header />}
+      {!selectedTournament && <Header search={search} setSearch={setSearch} />}
       <main className=" max-w-xl pb-10">
 
         {!selectedTournament ? (
 
-          tournamentData.map((t) => (
+          tournaments.filter((t) => t.title.toLowerCase().includes(search.toLowerCase())).map((t) => (
             <TournamentCard
               key={t.id}
               tournament={t}
               onClick={() => setSelectedTournament(t)}
+              addParticipant={addParticipant}
             />
           ))
 
-        ) 
-        :
-        (
-          <TournamentDetails
-            tournament={selectedTournament}
-            goBack={() => setSelectedTournament(null)}
-          />
+        )
+          :
+          (
+            <TournamentDetails
+              tournament={selectedTournament}
+              goBack={() => setSelectedTournament(null)}
+              addParticipant={addParticipant}
+            />
 
-        )}
+          )}
 
       </main>
 
